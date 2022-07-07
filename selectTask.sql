1481223c2e6a9fa64efcc190a9eadb8a38684a81
Shahrukh Sultan <getssultan@gmail.com>
2022-07-07 16:04:54 +0300

    select and where practice

---
+++ selectTask.sql
@@ -0,0 +1,5 @@
+select distinct first_name, last_name, email, address_id from customer;
+
+
+-- for distinct practice
+select distinct(rating) from film;
---
+++ whereTask.sql
@@ -0,0 +1,2 @@
+select email from customer 
+where first_name = 'Nancy' and last_name = 'Thomas';
